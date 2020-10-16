f584fdbe2ba50b570b72d09e9759b8d8dab26752
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-15 19:15:34 -0700

    modifiy connection to route to home page

--- config/orm.js
+++ config/orm.js
@@ -3,24 +3,31 @@ const connection = require("../config/connection.js");
 // Object for all our SQL statement functions.
 const orm = {
     selectAll: (tableInput, cb) => {
-     
+        const queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+
     },
 
-    insertOne: (table, newRowData, cb) => {
-      
-    },
-  
-    // Example of updateValues: { name: "panther", sleepy: true }
-    // Example of condition: { id: 1 }
-    updateOne: (table, updateValues, condition, cb) => {
-      
-    },
-    // Delete row(s) from table with given condition.
-    // Example condition: { id: 1 }
-    deleteOne: (table, condition, cb) => {
-     
-    },
-  };
-  
-  // Export the orm object
-  module.exports = orm;
+    // insertOne: (table, newRowData, cb) => {
+
+    // },
+
+    // // Example of updateValues: { name: "panther", sleepy: true }
+    // // Example of condition: { id: 1 }
+    // updateOne: (table, updateValues, condition, cb) => {
+
+    // },
+    // // Delete row(s) from table with given condition.
+    // // Example condition: { id: 1 }
+    // deleteOne: (table, condition, cb) => {
+
+    // },
+};
+
+// Export the orm object
+module.exports = orm;
