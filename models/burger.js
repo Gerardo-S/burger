aefd767d7a80276a67dbe7559788d1ccb90b37d0
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-15 19:03:37 -0700

    model for burger.js complete

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: (cb) => {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: (newBurger, cb) => {
+    orm.insertOne("burgers", newBurger, (res) => {
+      cb(res);
+    });
+  },
+  update: (burgerData, criteria, cb) => {
+    orm.updateOne("burgers", burgerData, criteria, (res) => {
+      cb(res);
+    });
+  },
+  delete: (condition, cb) => {
+    orm.deleteOne("burgers", condition, (res) => {
+      cb(res);
+    });
+  },
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
