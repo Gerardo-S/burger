ac05ccf3e8ed8e581afa124f45b17c2bfc1709dc
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-16 23:10:24 -0700

    Layout content  centered

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -14,6 +14,7 @@ router.get("/", (req, res) => {
       alt: "Cheese Burger",
       width: "10%",
       height: "10%",
+      
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
